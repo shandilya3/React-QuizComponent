4ae28feb3c02e03589628ef6492bf61e5c69282f
Shandilya <happy.shandilya@coxautoinc.com>
2018-07-31 21:29:10 -0600

    test

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import QuizQuestion from './QuizQuestion.js';
-let quizData = require('./quiz_data.json');
+import React, {Component} from 'react'
+import QuizQuestion from './QuizQuestion.js'
+let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
   constructor(props) {
